d585f8a95d96322a6e9b17a9c13b226676e252e0
GabrielFaustinoIT <90875131+GabrielFaustinoIT@users.noreply.github.com>
2021-09-29 20:12:04 -0300

    Update Participante.cs

--- Participante.cs
+++ Participante.cs
@@ -31,42 +31,14 @@ namespace projEventos
             this.nome = nome;
         }
 
-        public Vendedor()
-            : this(0, "", 0.00)
+        public Participante()
+            : this("", "",)
         {
         }
 
-        public void registrarVenda(int dia, Venda venda)
-        {
-            this.asVendas[dia - 1] = venda;
-        }
+      
 
-        public double valorVendas()
-        {
-            double totVendas = 0;
-            foreach (Venda v in this.asVendas)
-                totVendas += v.Valor;
-            return totVendas;
-        }
 
-        public double valorComissao()
-        {
-            return this.valorVendas() * (this.percComissao / 100);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return this.id.Equals(((Vendedor)obj).id);
-        }
-
-        public override string ToString()
-        {
-            return this.id.ToString() + " - " + 
-                   this.nome + " - " + 
-                   this.percComissao.ToString() + " - " +
-                   this.valorVendas().ToString() + " - " +
-                   this.valorComissao().ToString() + '\n';
-        }
 
 
     }
